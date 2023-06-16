2f45d4919206a6a9f9adfacef0b9a022b9ef9a75
Moiseev Kirill <kilkuruzzza@mail.ru>
2023-06-16 22:11:55 +0300

    ispravil

--- 63_new/Program.cs
+++ 63_new/Program.cs
@@ -10,20 +10,22 @@ using System;
 using static System.Console;
 Clear();
 Write("Введите N: ");
-int n=int.Parse(ReadLine());
-for (int i = 1; i <= n; i++){
+int n = int.Parse(ReadLine());
+for (int i = 1; i <= n; i++)
+{
     Write($"{i} ");
 }
 WriteLine();
 WriteLine(PrintNumbers(n));
 
-string PrintNumbers(int n){
+string PrintNumbers(int n)
+{
     if (n == 1)
     {
         WriteLine(1);
         return "1";
     }
-    string s = PrintNumbers(n - 1) + ", " + n.ToString();
+    string s = PrintNumbers(n - 1) + ", " + n.ToString();// $"{n}"
     WriteLine(s);
     return s;
 }
---
+++ 65_new/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(m, n));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start + 1, end));
+}
+/***Задача 67:** Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9*/
